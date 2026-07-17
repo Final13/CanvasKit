2d088c805881c3445c9c809603b1c8b7c7bc89d5
Ivan Matskevich <faxtalk666@gmail.com>
2026-07-17 12:40:47 +0300

    Change oferta

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
       cwd: "/var/www/evspc/data/www/evspc.com",
       env: {
         NODE_ENV: "production",
-        PORT: 3000,
+        PORT: 4047,
       },
       instances: 1,
       exec_mode: "fork",
