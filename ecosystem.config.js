d0e5ef39c2663c5e989f5749d1c30ac974f6a4ba
Ivan Matskevich <faxtalk666@gmail.com>
2026-07-18 14:49:43 +0300

    Fix ci env handle

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
   apps: [
     {
       name: "canvaskit",
-      script: "npm",
-      args: "run start",
+      script: "node_modules/next/dist/bin/next",
+      args: "start",
       cwd: "/var/www/evspc/data/www/evspc.com",
       env: {
         NODE_ENV: "production",
