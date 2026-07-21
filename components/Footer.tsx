8b503ca71e064ad84e0baff657308efc20913d5f
Ivan Matskevich <faxtalk666@gmail.com>
2026-07-21 12:11:37 +0300

    Add ya metric

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Script from "next/script";
 
 const links = [
   { label: "Условия использования", href: "/terms" },
@@ -81,6 +82,35 @@ export function Footer() {
           </p>
         </div>
       </div>
+
+      {/* Yandex.Metrika counter */}
+      <Script
+        id="yandex-metrika"
+        strategy="lazyOnload"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=110908888', 'ym');
+
+            ym(110908888, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+          `,
+        }}
+      />
+      <noscript>
+        <div>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src="https://mc.yandex.ru/watch/110908888"
+            style={{ position: "absolute", left: "-9999px" }}
+            alt=""
+          />
+        </div>
+      </noscript>
+      {/* /Yandex.Metrika counter */}
     </footer>
   );
 }
